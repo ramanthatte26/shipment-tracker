286636f2f7baa24431550c215d653ae5a8db11bc
ramanthatte26 <ramanthatte261@gmail.com>
2026-04-01 16:15:55 +0530

    CORS Fixed

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 const shipmentRoutes = require('./routes/shipments');
